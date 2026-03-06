9f753bc4090034bc50e7274f458289e9ea922bd5
deblur99 <deblur99@kakao.com>
2026-03-06 10:16:56 +0900

    Initial Commit

---
+++ AssetFlow/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  AssetFlow
+//
+//  Created by 한현민 on 3/6/26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
